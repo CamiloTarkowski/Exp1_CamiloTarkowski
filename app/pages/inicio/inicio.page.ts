5669b97961d7b312048d24c5671c2693e595cbb4
CamiloTarkowski <camilo.tarkowski@gmail.com>
2021-09-19 16:50:31 -0300

    page inicio y prueba de lista

--- app/pages/inicio/inicio.page.ts
+++ app/pages/inicio/inicio.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+
+interface Componente{
+  icon: string;
+  name: string;
+  redirecTo: string;
+}
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +14,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
-  constructor() { }
+  componentes : Componente[] = [
+    {
+      icon: 'home-outline',
+      name: 'Inicio',
+      redirecTo: '/inicio'
+    },
+    
+    {
+      icon: 'person-add-outline',
+      name: 'Registro',
+      redirecTo: '/registro'
+    },
+    {
+      icon:'bag-handle-outline',
+      name: 'Ver ofertas',
+      redirecTo:'/ofertas'
+    },
+    {
+      icon:'notifications-outline',
+      name: 'Notificaciones',
+      redirecTo:'/inicio'
+    },
+    {
+      icon:'heart-outline',
+      name: 'Favoritos',
+      redirecTo:'/inicio'
+    },
+    {
+      icon:'bag-check-outline',
+      name: 'Mis compras',
+      redirecTo:'/inicio'
+    }
+
+
+
+  ];
+
+  constructor(private menuController: MenuController) { }
 
   ngOnInit() {
   }
+
+  mostrarMenu(){
+    this.menuController.open('first');  }
 
 }
